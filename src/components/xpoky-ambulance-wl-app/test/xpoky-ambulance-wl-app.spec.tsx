59829fd64ebe077595be7f2b9cccf23de36fa7f4
Oliver Pokorný <opokorny@microstep-hdo.sk>
2025-05-14 11:54:06 +0200

    new testy

--- src/components/xpoky-ambulance-wl-app/test/xpoky-ambulance-wl-app.spec.tsx
+++ src/components/xpoky-ambulance-wl-app/test/xpoky-ambulance-wl-app.spec.tsx
@@ -5,13 +5,33 @@ describe('xpoky-ambulance-wl-app', () => {
   it('renders', async () => {
     const page = await newSpecPage({
       components: [XpokyAmbulanceWlApp],
-      html: `<xpoky-ambulance-wl-app></xpoky-ambulance-wl-app>`,
+      html: `<xpoky-ambulance-wl-app 
+        api-base="http://localhost:3333"
+        base-path="/">
+      </xpoky-ambulance-wl-app>`,
     });
 
     expect(page.root).toEqualHtml(`
-      <xpoky-ambulance-wl-app>
+      <xpoky-ambulance-wl-app api-base="http://localhost:3333" base-path="/">
         <mock:shadow-root>
-          <slot></slot>
+          <header>
+            <md-filled-select label="Logged User">
+              <md-icon slot="leading-icon">person</md-icon>
+            </md-filled-select>
+          </header>
+          <xpoky-ambulance-wl-list api-base="http://localhost:3333" logged-user-id="">
+            <mock:shadow-root>
+              <div class="appointments-container">
+                <h2>My Appointments</h2>
+                <md-list></md-list>
+                <h2>Created Appointments</h2>
+                <md-list></md-list>
+              </div>
+              <md-filled-icon-button class="add-button">
+                <md-icon>add</md-icon>
+              </md-filled-icon-button>
+            </mock:shadow-root>
+          </xpoky-ambulance-wl-list>
         </mock:shadow-root>
       </xpoky-ambulance-wl-app>
     `);
--- src/components/xpoky-ambulance-wl-list/test/xpoky-ambulance-wl-list.spec.tsx
+++ src/components/xpoky-ambulance-wl-list/test/xpoky-ambulance-wl-list.spec.tsx
@@ -5,13 +5,21 @@ describe('xpoky-ambulance-wl-list', () => {
   it('renders', async () => {
     const page = await newSpecPage({
       components: [XpokyAmbulanceWlList],
-      html: `<xpoky-ambulance-wl-list></xpoky-ambulance-wl-list>`,
+      html: `<xpoky-ambulance-wl-list api-base="http://localhost:3333" logged-user-id="test-user"></xpoky-ambulance-wl-list>`,
     });
 
     expect(page.root).toEqualHtml(`
-      <xpoky-ambulance-wl-list>
+      <xpoky-ambulance-wl-list api-base="http://localhost:3333" logged-user-id="test-user">
         <mock:shadow-root>
-          <slot></slot>
+          <div class="appointments-container">
+            <h2>My Appointments</h2>
+            <md-list></md-list>
+            <h2>Created Appointments</h2>
+            <md-list></md-list>
+          </div>
+          <md-filled-icon-button class="add-button">
+            <md-icon>add</md-icon>
+          </md-filled-icon-button>
         </mock:shadow-root>
       </xpoky-ambulance-wl-list>
     `);
